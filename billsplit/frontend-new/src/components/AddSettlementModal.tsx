03483d28ebbfd9c7fb28b5509179d32bc17704ba
fhke <fhke@protonmail.com>
2026-04-04 20:31:34 +0100

    feat: add GroupDetail page with balances, history, and pagination

---
+++ billsplit/frontend-new/src/components/AddSettlementModal.tsx
@@ -0,0 +1,3 @@
+import { type Group } from '@/api'
+interface Props { group: Group; onClose: () => void; onSaved: () => void }
+export default function AddSettlementModal(_props: Props) { return null }
